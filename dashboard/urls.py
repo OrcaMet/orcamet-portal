4b0c747041f6841fc43c432b09bb26137e725e94
OrcaMet <stevecar55.sc@gmail.com>
2026-03-02 07:35:50 +0000

    Add new routes for weather map and JSON data

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,6 +5,8 @@ app_name = "dashboard"
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("map/", views.weather_map, name="weather_map"),
+    path("map/sites.json", views.map_sites_json, name="map_sites_json"),
     path("site/<int:site_id>/", views.site_detail, name="site_detail"),
     path("site/<int:site_id>/chart-data/", views.forecast_chart_data, name="chart_data"),
 ]
